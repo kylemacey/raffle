dd33fadee7c836d15a5b9460595d576559f7beeb
Kyle Macey <shout@kylemacey.com>
2024-11-27 17:45:09 -0500

    Fix redis... I hope

---
+++ config/initializers/redis.rb
@@ -0,0 +1,4 @@
+# I guess Heroku uses self-signed SSL certificates
+if Rails.env.production?
+  $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+end
