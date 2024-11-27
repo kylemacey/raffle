b34281898198488fcd9618a1508e490810c61674
Kyle Macey <shout@kylemacey.com>
2024-11-27 17:59:14 -0500

    If this works, I'll buy a lifetime subscription to chatgpt

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,8 @@
 # I guess Heroku uses self-signed SSL certificates
 
-$redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+require 'redis'
 
-
+Redis.current = Redis.new(
+  url: ENV['REDIS_URL'], # Or your Redis URL
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+)
