d3c11610dc14e47148c8284b88dfa21d55d0bd5c
Kyle Macey <shout@kylemacey.com>
2024-11-27 18:01:56 -0500

    StackOverflow wins again?

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,8 @@
 # I guess Heroku uses self-signed SSL certificates
 
-require 'redis'
+# require 'redis'
 
-Redis.current = Redis.new(
-  url: ENV['REDIS_URL'], # Or your Redis URL
-  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
-)
+# Redis.current = Redis.new(
+#   url: ENV['REDIS_URL'], # Or your Redis URL
+#   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+# )
