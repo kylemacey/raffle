0aba04de7995437966ae8379769238057b5863cb
Kyle Macey <shout@kylemacey.com>
2025-06-22 18:27:43 -0400

    sign in pad

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :pin, presence: true,
+                  length: { is: 4, message: "must be exactly 4 digits" },
+                  format: { with: /\A\d{4}\z/, message: "must contain only numbers" }
 end
